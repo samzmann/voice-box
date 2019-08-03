3466cb325373498b13436d77e5e35594b1ad204a
Samuel Mann <samuelcmann@gmail.com>
2019-08-04 00:14:07 +0200

    enable record and play audio

--- src/pages/Record/index.tsx
+++ src/pages/Record/index.tsx
@@ -1,17 +1,82 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { RouteComponentProps } from '@reach/router'
 import firebase from '../../firebase'
+
+// taken from https://medium.com/@bryanjenningz/how-to-record-and-play-audio-in-javascript-faa1b2b3e49b
+const recordAudio = () =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+      const mediaRecorder = new MediaRecorder(stream)
+      let audioChunks: Array<any> = []
+
+      mediaRecorder.addEventListener(
+        'dataavailable',
+        // @ts-ignore
+        (event: { data: Blob }) => {
+          audioChunks.push(event.data)
+        }
+      )
+
+      const start = () => {
+        audioChunks = []
+        mediaRecorder.start()
+      }
+
+      const stop = () =>
+        new Promise(resolve => {
+          mediaRecorder.addEventListener('stop', () => {
+            const audioBlob = new Blob(audioChunks)
+            const audioUrl = URL.createObjectURL(audioBlob)
+            const audio = new Audio(audioUrl)
+
+            const play = () => {
+              audio.play()
+            }
+
+            resolve({ audioBlob, audioUrl, play })
+          })
+
+          mediaRecorder.stop()
+        })
+
+      resolve({ start, stop })
+    } catch (error) {
+      console.log('error:', error)
+      reject(error)
+    }
+  })
 
 interface RecordPageProps extends RouteComponentProps {}
 
 const Record: React.FC<RecordPageProps> = () => {
+  const [recorder, setRecorder] = useState()
   const [isRecording, setIsRecording] = useState(false)
+  const [audio, setAudio] = useState(null)
 
-  const handleRecord = () => {
+  useEffect(() => {
+    const initializeRecorder = async () => {
+      const newRecorder = await recordAudio()
+      setRecorder(newRecorder)
+    }
+    initializeRecorder()
+  }, [])
+
+  const handleRecord = async () => {
     if (isRecording) {
       setIsRecording(false)
+      const newAudio = await recorder.stop()
+      setAudio(newAudio)
     } else {
       setIsRecording(true)
+      setAudio(null)
+      recorder.start()
+    }
+  }
+
+  const playRecording = () => {
+    if (audio && audio.play) {
+      audio.play()
     }
   }
 
@@ -24,6 +89,9 @@ const Record: React.FC<RecordPageProps> = () => {
       <h1>Record Page</h1>
       <button onClick={handleRecord}>
         {isRecording ? 'Stop' : 'Start'} recording
+      </button>
+      <button onClick={playRecording} disabled={isRecording}>
+        Play recording
       </button>
       <button onClick={saveRecording} disabled={isRecording}>
         Save recording
