33e72a8b8dcf122b0b81476bac9d97a1d533be69
Samuel Mann <samuelcmann@gmail.com>
2019-08-03 22:24:11 +0200

    lay base for record interface

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,7 @@
 import app from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage'
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -17,6 +18,7 @@ app.initializeApp(config)
 const firebase = {
   auth: app.auth(),
   db: app.firestore(),
+  storage: app.storage(),
 }
 
 export default firebase
--- src/pages/Record/index.tsx
+++ src/pages/Record/index.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { RouteComponentProps } from '@reach/router';
+import React, { useState } from 'react'
+import { RouteComponentProps } from '@reach/router'
+import firebase from '../../firebase'
 
-interface RecordPageProps extends RouteComponentProps {
-
-}
+interface RecordPageProps extends RouteComponentProps {}
 
 const Record: React.FC<RecordPageProps> = () => {
+  const [isRecording, setIsRecording] = useState(false)
+
+  const handleRecord = () => {
+    if (isRecording) {
+      setIsRecording(false)
+    } else {
+      setIsRecording(true)
+    }
+  }
+
+  const saveRecording = () => {
+    // const uploadTask = firebase.storage.ref().put()
+  }
+
   return (
+    <div>
       <h1>Record Page</h1>
+      <button onClick={handleRecord}>
+        {isRecording ? 'Stop' : 'Start'} recording
+      </button>
+      <button onClick={saveRecording} disabled={isRecording}>
+        Save recording
+      </button>
+    </div>
   )
-};
+}
 
 export default Record
