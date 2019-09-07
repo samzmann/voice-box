b2381d6588a27419a9cc1b2e1304c07bf54a7420
Samuel Mann <samuelcmann@gmail.com>
2019-09-07 15:53:46 +0200

    get audio from storage and pass to <audio> element. Read is working!!!

--- src/pages/Message/index.tsx
+++ src/pages/Message/index.tsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { RouteComponentProps } from '@reach/router'
+import firebase from '../../firebase'
 
 interface MessagePageProps extends RouteComponentProps {
   messageId?: string
 }
 
 const Message: React.FC<MessagePageProps> = ({ messageId }) => {
+  const [loading, setLoading] = useState(false)
+  const [messageURL, setMessageURL] = useState(null)
+
+  useEffect(() => {
+    const getMessageURL = async () => {
+      setLoading(true)
+      try {
+        if (messageId) {
+          const msgURL = await firebase.storage
+            .ref()
+            .child(`recordings/${messageId}`)
+            .getDownloadURL()
+          console.log(msgURL)
+
+          setMessageURL(msgURL)
+          setLoading(false)
+        }
+      } catch (error) {
+        console.log(error)
+        setLoading(false)
+      }
+    }
+
+    getMessageURL()
+  }, [messageId])
+
   return (
     <div>
       <h1>Message Page</h1>
       <div>Message id: {messageId}</div>
+      {loading && <div>Loading...</div>}
+      {messageURL && <audio controls src={messageURL} />}
     </div>
   )
 }
