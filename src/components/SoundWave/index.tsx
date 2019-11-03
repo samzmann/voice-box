a2786f3ceb38b19b87ddcb816b3521dbe0306499
Samuel Mann <samuelcmann@gmail.com>
2019-11-03 11:32:24 +0100

    make audioWaveform component and sketch with mock data

--- src/components/SoundWave/index.tsx
+++ src/components/SoundWave/index.tsx
@@ -1,17 +1,38 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 // @ts-ignore
 import P5Wrapper from 'react-p5-wrapper'
-import soundwave from '../../p5/soundwave'
+import audioWaveform from '../../p5/audioWaveform'
 
 const SoundWave: React.FC = () => {
   const canvasHolderRef = useRef<HTMLDivElement>(null)
+
+  const [mockData, setMockData] = useState([])
+
+  useEffect(() => {
+    let isMounted = true
+    const generateMockData = () => {
+      const newData = []
+      for (let i = 0; i < 1024; i++) {
+        newData.push(Math.random() * 255)
+      }
+      isMounted && setMockData(newData)
+    }
+
+    generateMockData()
+    return () => (isMounted = false)
+  }, [])
+
   return (
     <div
       style={{ height: 50, border: '1px solid yellow' }}
       ref={canvasHolderRef}
     >
-      <P5Wrapper sketch={soundwave} parentDivRef={canvasHolderRef} />
+      <P5Wrapper
+        sketch={audioWaveform}
+        parentDivRef={canvasHolderRef}
+        waveformData={mockData}
+      />
     </div>
   )
 }
---
+++ src/p5/audioWaveform.ts
@@ -0,0 +1,86 @@
+import 'p5/lib/addons/p5.sound'
+import { color } from '../constants/color'
+
+const audioWaveform = (p: any) => {
+  let canvas
+  let canvasInitialized = false
+  let parentDivRef: HTMLDivElement = null
+
+  let waveformData: number[] = []
+  let normalizedData: number[] = []
+
+  p.setup = () => {
+    canvas = p.createCanvas(0, 0)
+    p.background(color.DarkGrey)
+  }
+
+  p.draw = () => {
+    p.background(color.DarkGrey)
+    p.fill(color.LightGrey)
+    p.noStroke()
+
+    const barWidth = 5
+    const gutterWidth = 2
+    const numRects = p.floor(p.width / (barWidth + gutterWidth))
+    const actualGutter = (p.width - numRects * barWidth) / (numRects + 1)
+
+    // normalise waveformData to the actual amount of rects.
+    // eg. If waveformData has 10 value and numRects = 3, this routine
+    // will produce an array with three values representing the average
+    if (normalizedData.length !== numRects) {
+      normalizedData = []
+
+      // generate a temporary array of length = numRects,
+      // each value is an array of values in waveformArray
+      // with the corresponding targetIndex
+      const temp = waveformData.reduce((acc, val, index) => {
+        const targetIndex = Math.floor((index / waveformData.length) * numRects)
+        const targetArray = acc[targetIndex] || []
+        targetArray.push(val)
+        acc[targetIndex] = targetArray
+        return acc
+      }, [])
+
+      // generate the average of each value array in temp array
+      for (let i = 0; i < temp.length; i++) {
+        normalizedData[i] =
+          temp[i].reduce((a: number, b: number) => a + b, 0) / temp[i].length
+      }
+    }
+
+    // render the bars
+    for (let i = 0; i < normalizedData.length; i++) {
+      const posX = actualGutter + i * (barWidth + actualGutter)
+      const posY = p.height
+      const randHeight = p.map(normalizedData[i], 0, 255, 0, p.height)
+      const barHeight = -(posY - (posY - randHeight))
+      p.rect(posX, posY, barWidth, barHeight)
+    }
+  }
+
+  p.windowResized = () => {
+    const { clientWidth, clientHeight } = parentDivRef
+    p.resizeCanvas(clientWidth, clientHeight)
+  }
+
+  p.myCustomRedrawAccordingToNewPropsHandler = (newProps: any) => {
+    console.log('newProps', newProps)
+
+    if (!canvasInitialized) {
+      const { parentDivRef: parentDivRefProp } = newProps
+      parentDivRef = parentDivRefProp.current
+
+      if (parentDivRef) {
+        const { clientWidth, clientHeight } = parentDivRef
+        console.log({ clientWidth, clientHeight })
+        p.resizeCanvas(clientWidth, clientHeight)
+        canvasInitialized = true
+      }
+    }
+
+    const { waveformData: waveformDataFromProps } = newProps
+    waveformData = waveformDataFromProps
+  }
+}
+
+export default audioWaveform
