b152a89c655c8eece33fc46b7a20ca90f7ee6682
Samuel Mann <samuelcmann@gmail.com>
2019-11-23 18:40:17 +0100

    style the navbar

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,16 +1,57 @@
 import React from 'react'
 import { Link } from '@reach/router'
+import styled from 'styled-components'
+import { padding } from '../../constants/padding'
+import { ButtonStandard } from '../../elements/buttons/ButtonStandard'
+
+const Container = styled.div`
+  flex-direction: row;
+  padding: ${padding.m}px;
+`
+
+const Left = styled.div`
+  flex: 1;
+  flex-direction: row;
+`
+
+const Right = styled.div`
+  flex: 1;
+  align-items: flex-end;
+`
+
+const Space = styled.div`
+  width: ${padding.s}px;
+`
+
+const ButtonContainer = styled.div`
+  align-self: center;
+`
+// TODO: fix mobile layout, ButtonStandard should be one line only, not sure if optional onClick is a good idea
 
 const Nav = () => {
   return (
-    <div>
-      <div>
-        <Link to="/">Home</Link>
-      </div>
-      <div>
-        <Link to="/record">Record</Link>
-      </div>
-    </div>
+    <Container>
+      <Left>
+        <ButtonContainer>
+          <Link to="/">
+            <ButtonStandard>Home</ButtonStandard>
+          </Link>
+        </ButtonContainer>
+        <Space />
+        <ButtonContainer>
+          <Link to="/record">
+            <ButtonStandard>Record</ButtonStandard>
+          </Link>
+        </ButtonContainer>
+      </Left>
+      <Right>
+        <ButtonContainer style={{ alignSelf: 'flex-end' }}>
+          <Link to="/signup">
+            <ButtonStandard>Start your channel</ButtonStandard>
+          </Link>
+        </ButtonContainer>
+      </Right>
+    </Container>
   )
 }
 
--- src/elements/buttons/ButtonStandard.tsx
+++ src/elements/buttons/ButtonStandard.tsx
@@ -24,7 +24,7 @@ const Container = styled.div`
 `
 
 interface ButtonProps {
-  onClick: (event: React.MouseEvent<HTMLDivElement>) => void
+  onClick?: (event: React.MouseEvent<HTMLDivElement>) => void
   disabled?: boolean
 }
 
