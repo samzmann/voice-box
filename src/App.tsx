9ad5ceb9d6029a610aacc60dd86455077f4e6fce
Samuel Mann <samuelcmann@gmail.com>
2019-12-07 10:22:22 +0100

    rm useless firebase context, enable anonymous auth and AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import { Router } from '@reach/router'
+import { AuthContext, useAuth } from './firebase/auth'
 
 import Nav from './components/Nav'
-
 import Home from './pages/Home'
 import Record from './pages/Record'
 import Message from './pages/Message'
 import Signup from './pages/Signup'
 
 const App: React.FC = () => {
+  const { authUser, authInitialized } = useAuth()
+
   return (
-    <div>
-      <Nav />
-      <Router>
-        <Home path="/" />
-        <Signup path="/signup" />
-        <Record path="/record" />
-        <Message path="/:shortId" />
-      </Router>
-    </div>
+    <AuthContext.Provider value={{ authUser, authInitialized }}>
+      <div>
+        <Nav />
+        <Router>
+          <Home path="/" />
+          <Signup path="/signup" />
+          <Record path="/record" />
+          <Message path="/:shortId" />
+        </Router>
+      </div>
+    </AuthContext.Provider>
   )
 }
 
--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,23 +1,51 @@
-import { useState, useEffect, SetStateAction } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import firebase from './index'
 import { User } from 'firebase'
 
-export const useAuth = () => {
+type AuthContextType = {
+  authUser: User | null
+  authInitialized: boolean
+}
+
+export const AuthContext = React.createContext<AuthContextType>({
+  authUser: null,
+  authInitialized: false,
+})
+
+/**
+ * useAuth
+ *
+ * ⚠️ Should only be mounted once on top level component (App.tsx).
+ *
+ * Listens to auth state changes, and returns auth user.
+ * If no auth user is returned by onAuthStateChanged, we create a
+ * new anonymous user and return that
+ *
+ * @returns { AuthContextType }
+ * */
+export const useAuth = (): AuthContextType => {
   const [authUser, setAuthUser] = useState(firebase.auth.currentUser)
   const [authInitialized, setAuthInitialized] = useState(
     !!firebase.auth.currentUser
   )
 
   useEffect(() => {
-    console.log('running effect')
-    firebase.auth.onAuthStateChanged(
-      (firebaseAuthUser: SetStateAction<User>) => {
-        console.log('firebaseAuthUser', firebaseAuthUser)
-        setAuthUser(firebaseAuthUser)
-        setAuthInitialized(true)
+    let isMounted = true
+
+    firebase.auth.onAuthStateChanged((firebaseAuthUser: User) => {
+      console.log('firebaseAuthUser', firebaseAuthUser)
+      isMounted && setAuthUser(firebaseAuthUser)
+      isMounted && setAuthInitialized(true)
+
+      if (!firebaseAuthUser) {
+        firebase.auth.signInAnonymously().catch(console.error)
       }
-    )
+    })
+
+    return () => {
+      isMounted = false
+    }
   }, [])
 
   return { authUser, authInitialized }
